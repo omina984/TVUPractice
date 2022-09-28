a0e67ea5bfd77b87f13bb6297ac066d515d9438b
omina984 <omina984@yahoo.com>
2022-09-28 18:30:49 +0330

    ok

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,1 +1,15 @@
-<b>my footer</b>
+<style>
+    .MyStyle
+    {
+        font-family: B Nazanin;
+        font-size: 30px;
+        color: black;
+        text-align: center;
+        /* border: 1px solid black */
+    }
+</style>
+
+<div class="MyStyle">
+    <b >فوتر من</b>
+</div>
+
